32e6fc5cb4f3734443049de83019bca25b0408e7
Alex Tester <lex.tester93@gmail.com>
2023-07-12 12:42:41 -0400

    Added function for displaying time and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,8 @@ function dashboard(event) {
     const cityName = inputCityEl.value;
     getWeather(cityName);
 }
+// Handles displaying the time and date
+function displayTime() {
+    const timeDisplayEl = document.querySelector("#time-display"); // Element for displaying time
+    const rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.textContent = rightNow;
